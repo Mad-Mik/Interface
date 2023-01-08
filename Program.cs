3fdc5061ccfcf506ecc3fc03d53437edbcb49632
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-08 13:36:00 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Interface
+{
+    //interfacei bolor andamnery(methodnery...) default public en!!!
+
+    interface IDataProvider
+    {
+        string GetData();
+    }
+
+    interface IDataProcessor
+    {
+        void ProcessData(IDataProvider dataProvider);
+    }
+
+    class ConsoleDataProcessor : IDataProcessor
+    {
+        public void ProcessData(IDataProvider dataProvider)
+        {
+            Console.WriteLine(dataProvider.GetData());
+        }
+    }
+
+    class DbDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Data from DB";
+        }
+    }
+
+    class FileDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Data from File";
+        }
+    }
+
+    class APIDataProvider : IDataProvider
+    {
+        public string GetData()
+        {
+            return "Data from API";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDataProcessor dataProcessor = new ConsoleDataProcessor();
+            dataProcessor.ProcessData(new DbDataProvider());
+            dataProcessor.ProcessData(new FileDataProvider());
+            dataProcessor.ProcessData(new APIDataProvider());
+
+            Console.ReadLine();
+        }
+    }
+}
